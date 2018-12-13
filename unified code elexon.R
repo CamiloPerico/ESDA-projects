fb52599ef81fa7951ae26490095e0d3720cae6d2
Felipe Cortes Gomez <felipe@eduroam-int-dhcp-97-235-55.ucl.ac.uk>
2018-12-13 17:45:14 +0000

    modifications elexon full code

--- unified code elexon.R
+++ unified code elexon.R
@@ -7,22 +7,25 @@ library(lubridate)
 APIKey <- 'x11mhwta8mlnbb8'
 
 #Extract data from generatio unit
-service <- 'B1610'
+service_gen <- 'B1610'
 Period <- '*'
 ServiceType <- 'csv'
 
-start <- as.Date("01-01-18",format="%d-%m-%y")
-end   <- as.Date("02-01-18",format="%d-%m-%y")
+#Define the range that we want data from:
+start <- as.Date("01-02-18",format="%d-%m-%y")
+end   <- as.Date("02-02-18",format="%d-%m-%y")
 
 theDate <- start
+#Define the matrix that will hold the data extracted
 generationrange <- data.frame(matrix(ncol = 19, nrow = 0))
+
 while (theDate <= end)
 {
-  url3 <- capture.output(cat('https://api.bmreports.com/BMRS/', service, '/v1?APIKey=', APIKey,
+  url <- capture.output(cat('https://api.bmreports.com/BMRS/', service_gen, '/v1?APIKey=', APIKey,
                              '&SettlementDate=', format(theDate),
                              '&Period=', Period,
                              '&ServiceType=', ServiceType, sep = ''))
-  generationloop_raw <- GET(url = url3)
+  generationloop_raw <- GET(url = url)
   
   generationloop_csv <- rawToChar(generationloop_raw$content)
   generationloop_HH <- read.table(text = generationloop_csv, sep=',', fill = TRUE)
@@ -34,33 +37,14 @@ while (theDate <= end)
   rownames(generationloop_HH) <- NULL
   generationloop_HH <- generationloop_HH[-c(nrow(generationloop_HH)),]
   
-  print(url3)
+  print(url)
   
   generationrange <- rbind(generationrange, generationloop_HH) 
   theDate <- theDate+1
 }
-colnames(generationrange) <-   c("*Document Type",
-                                 "Business Type",
-                                 "Process Type",
-                                 "Time Series ID",
-                                 "Quantity", 
-                                 "Curve Type",
-                                 "Resolution",
-                                 "Settlement Date",
-                                 "Settlement Period",
-                                 "Power System Resource  Type",
-                                 "Registered Resource EIC Code",
-                                 "Market Generation Unit EIC Code",
-                                 "Market Generation BMU Id", 
-                                 "Market Generation NGC BMU Id",
-                                 "BM Unit ID",
-                                 "NGC BM Unit ID",
-                                 "Active Flag",
-                                 "Document ID",
-                                 "Document RevNum")
 
 #Extract the price of Market data - NOT RUNNING WELL AT LEAST FOR ME
-service6 <- 'MID'
+service <- 'MID'
 Bal_Mkt_Price <- data.frame(matrix(ncol = 6, nrow = 0))
 
 theDate2 <- start
@@ -109,3 +93,22 @@ names(generation_price)[11] <- "VCode"
 generatio_unified <- left_join(generation_price, EIC_codes_raw, by = "VCode")
 
 write.csv(generatio_unified,'test.csv')
+colnames(generationrange) <-   c("*Document Type",
+                                 "Business Type",
+                                 "Process Type",
+                                 "Time Series ID",
+                                 "Quantity", 
+                                 "Curve Type",
+                                 "Resolution",
+                                 "Settlement Date",
+                                 "Settlement Period",
+                                 "Power System Resource  Type",
+                                 "Registered Resource EIC Code",
+                                 "Market Generation Unit EIC Code",
+                                 "Market Generation BMU Id", 
+                                 "Market Generation NGC BMU Id",
+                                 "BM Unit ID",
+                                 "NGC BM Unit ID",
+                                 "Active Flag",
+                                 "Document ID",
+                                 "Document RevNum")
