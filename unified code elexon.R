10895510d0102c570f53f80ee159a0c5dbf8b5f2
Felipe Cortes Gomez <felipe@eduroam-int-dhcp-97-254-101.ucl.ac.uk>
2019-01-13 12:53:49 +0000

    elexon modifications with new price data pulling

--- unified code elexon.R
+++ unified code elexon.R
@@ -12,8 +12,8 @@ Period <- '*'
 ServiceType <- 'csv'
 
 #Define the range that we want data from:
-start <- as.Date("01-03-18",format="%d-%m-%y")
-end   <- as.Date("31-03-18",format="%d-%m-%y")
+start <- as.Date("01-01-17",format="%d-%m-%y")
+end   <- as.Date("31-12-17",format="%d-%m-%y")
 
 theDate <- start
 #Define the matrix that will hold the data extracted
@@ -110,3 +110,39 @@ write.csv(generation_unified,'generation_price_unit_2017.csv')
 #Code to evaluate duplicity
 duplicated(EIC_codes_raw$`Registered Resource EIC Code`) | duplicated(EIC_codes_raw$`Registered Resource EIC Code`, fromLast = TRUE)
 
+
+# New Prices Data pulling -------------------------------------------------
+service_price2 <- 'B1770'
+
+price_long <- data.frame(matrix(ncol = 15, nrow = 0))
+
+theDate3 <- start
+
+while (theDate3 <= end)
+{
+  FromSettlementDate1 <- theDate3
+  ToSettlementDate <- theDate3
+  url7 <- capture.output(cat('https://api.bmreports.com/BMRS/', service_price2, '/v1?APIKey=', APIKey,
+                             '&SettlementDate=', format(FromSettlementDate1),
+                             '&Period=', Period,
+                             '&ServiceType=', ServiceType, sep = ''))
+  
+  price_new_raw <- GET(url = url7) 
+  
+  price_new_csv <- rawToChar(price_new_raw$content)
+  price_loop <- read.table(text =price_new_csv, sep=',', fill = TRUE) 
+  price_loop <-price_loop[-c(1:4),]
+  rownames(price_loop) <- NULL
+  colnames(price_loop) <- as.character(unlist(price_loop[1,]))
+  price_loop <-price_loop[-1,]
+  price_loop <-price_loop[-c(nrow(price_loop)),]
+  rownames(price_loop) <- NULL
+  print(url7)
+  
+  price_long <- rbind(price_long, price_loop)
+  theDate3 <- theDate3+1
+}
+
+
+
+write.csv(price_long,'price_market_2017.csv')
